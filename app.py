1c6572db0fc395ba6a005378cc4d09b62a8208a7
Sean Collins <seanc@seancotech.com>
2017-10-19 07:43:14 -0500

    Add port param

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from routes import base_routes
+from config import port
+
 
 app = Flask(__name__)
 app.register_blueprint(base_routes)
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0', port=port)
 
--- config.py
+++ config.py
@@ -7,3 +7,6 @@ MUSIC_DIR = '/var/ambient_audio/'
 # Base volume (0.0 to 1.0)
 VOLUME = 0.4
 
+# Port to bind to
+port = 3251
+
